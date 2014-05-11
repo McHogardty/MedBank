15d5d42181c1c754702650f1db8f8e015c0bb19f
jet <michaelhagarty@gmail.com>
2014-05-12 00:12:47 +1000

    Various updates including a new way to enter explanations.

--- questions/forms.py
+++ questions/forms.py
@@ -5,7 +5,7 @@ from django.utils.encoding import force_text
 from django.db import models
 
 from medbank import bsforms
-from .models import Question, TeachingActivity, TeachingBlock, TeachingBlockYear, Student, Comment, TeachingActivityYear, QuizSpecification
+from .models import Question, TeachingActivity, TeachingBlock, TeachingBlockYear, Student, Comment, TeachingActivityYear, QuizSpecification, Reason
 
 import string
 import json
@@ -15,11 +15,11 @@ import datetime
 class QuestionOptionsWidget(forms.MultiWidget):
     def __init__(self, attrs=None):
         widgets = [
-            forms.TextInput(attrs={'class': ''}),
-            forms.TextInput(attrs={'class': ''}),
-            forms.TextInput(attrs={'class': ''}),
-            forms.TextInput(attrs={'class': ''}),
-            forms.TextInput(attrs={'class': ''}),
+            forms.TextInput(),
+            forms.TextInput(),
+            forms.TextInput(),
+            forms.TextInput(),
+            forms.TextInput(),
         ]
         super(QuestionOptionsWidget, self).__init__(widgets, attrs)
 
@@ -61,6 +61,52 @@ class QuestionOptionsWidget(forms.MultiWidget):
         return mark_safe(self.format_output(output))
 
 
+class QuestionOptionWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        widgets = [
+            forms.TextInput(),
+            forms.TextInput(),
+        ]
+        super(QuestionOptionWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if not value:
+            return []
+        j = json.loads(value)
+        return [j[x] for x in string.ascii_uppercase[:len(j)]]
+
+    def render(self, name, value, attrs=None):
+        if self.is_localized:
+            for widget in self.widgets:
+                widget.is_localized = self.is_localized
+
+        if not isinstance(value, list):
+            value = self.decompress(value)
+
+        output = ['<div class="options_table span6"><table style="width:100%;">']
+        final_attrs = self.build_attrs(attrs)
+        c = final_attrs.get('class', '')
+        if c:
+            c += " input-block-level"
+        else:
+            c = "input-block-level"
+        final_attrs['class'] = c
+        id_ = final_attrs.get('id', None)
+        alphabet = ['Text', 'Explanation']
+        for i, widget in enumerate(self.widgets):
+            try:
+                widget_value = value[i]
+            except IndexError:
+                widget_value = None
+            if id_:
+                final_attrs = dict(final_attrs, id='%s_%s' % (id_, alphabet[i]))
+            output.append('<tr><td class="option-label">%s</td><td class="">' % (alphabet[i], ))
+            output.append(widget.render(name + '_%s' % i, widget_value, final_attrs))
+            output.append('</td></tr>')
+        output.append('</table></div>')
+        return mark_safe(self.format_output(output))
+
+
 class QuestionOptionsField(forms.MultiValueField):
     widget = QuestionOptionsWidget
 
@@ -70,14 +116,34 @@ class QuestionOptionsField(forms.MultiValueField):
             forms.CharField(),
             forms.CharField(),
             forms.CharField(),
-            forms.CharField()
+            forms.CharField(),
         ]
         super(QuestionOptionsField, self).__init__(fields, *args, **kwargs)
 
         self.is_question_options_field = True
 
     def compress(self, values):
+        if not values:
+            values = [u''] * 5
         return json.dumps(dict(zip(string.ascii_uppercase[:len(values)], values)))
+
+ANSWER_CHOICES = ((x, x) for x in string.ascii_uppercase[:5])
+
+
+class QuestionOptionField(forms.MultiValueField):
+    widget = QuestionOptionWidget
+
+    def __init__(self, *args, **kwargs):
+        fields = [
+            forms.CharField(),
+            forms.CharField(),
+        ]
+        super(QuestionOptionField, self).__init__(fields, *args, **kwargs)
+
+        self.is_question_options_field = True
+
+    def compress(self, values):
+        return {'text': values[0], 'explanation': values[1]}
 
 ANSWER_CHOICES = ((x, x) for x in string.ascii_uppercase[:5])
 
@@ -88,8 +154,9 @@ class NewQuestionForm(bsforms.NewBootstrapModelForm):
     #body = forms.CharField(label="Question body", widget=bsforms.RichTextarea())
     options = QuestionOptionsField()
     answer = forms.ChoiceField(choices=ANSWER_CHOICES, widget=forms.Select())
-    explanation = forms.CharField(widget=forms.Textarea())
+    #explanation = forms.CharField(widget=forms.Textarea())
     #explanation = forms.CharField(widget=bsforms.RichTextarea())
+    explanation = QuestionOptionsField(required=False, help_text="Please explain why the answer is correct. You can also explain why the other answers are incorrect.")
     creator = forms.ModelChoiceField(queryset=Student.objects.all(), widget=forms.HiddenInput())
     teaching_activity_year = forms.ModelChoiceField(queryset=TeachingActivityYear.objects.all(), widget=forms.HiddenInput())
 
@@ -98,15 +165,29 @@ class NewQuestionForm(bsforms.NewBootstrapModelForm):
         if admin:
             self.fields['reason'] = forms.CharField(label='Reason for editing', widget=forms.Textarea(), help_text="This reason will be sent in an email to the question writer. Be nice! (and please use proper grammar)")
 
+    def clean(self):
+        answer = self.cleaned_data['answer']
+        if answer:
+            explanations = json.loads(self.cleaned_data['explanation'])
+            if not explanations[answer]:
+                extra = "also " if any(v for k, v in explanations.iteritems() if k != answer) else ""
+                self._errors['explanation'] = self.error_class(["You must %swrite an explanation for the answer, %s." % (extra, answer)])
+        return super(NewQuestionForm, self).clean()
+
     class Meta:
         model = Question
-        exclude = ('status', 'approver')
+        exclude = ('status', 'approver', 'suitable_for_faculty', 'suitable_for_quiz', 'requires_special_formatting')
 
 
 class TeachingActivityValidationForm(bsforms.BootstrapHorizontalModelForm):
     class Meta:
         model = TeachingActivity
-        exclude = ('block')
+
+
+class TeachingActivityYearValidationForm(bsforms.BootstrapHorizontalModelForm):
+    class Meta:
+        model = TeachingActivityYear
+        exclude = ('teaching_activity', 'block_year')
 
 
 class NewTeachingActivityForm(bsforms.BootstrapHorizontalModelForm):
@@ -114,8 +195,14 @@ class NewTeachingActivityForm(bsforms.BootstrapHorizontalModelForm):
         model = TeachingActivity
 
 
-class TeachingActivityBulkUploadForm(bsforms.BootstrapHorizontalForm):
-    ta_file = forms.FileField()
+class NewTeachingActivityYearForm(bsforms.BootstrapHorizontalModelForm):
+    class Meta:
+        model = TeachingActivityYear
+        exclude = ('teaching_activity',)
+
+
+class TeachingActivityBulkUploadForm(bsforms.NewBootstrapForm):
+    ta_file = forms.FileField(label="Activity Information File")
     year = forms.IntegerField()
 
 
@@ -136,7 +223,7 @@ class NewTeachingBlockYearForm(bsforms.NewBootstrapModelForm):
 #    end = forms.DateField(widget=forms.TextInput(
 #        attrs={'class': 'datepicker', 'data-date-format': 'dd/mm/yyyy'}
 #    ), help_text="The last day that students can assign themselves to activities in this block.")
-    block = forms.ModelChoiceField(queryset=TeachingBlock.objects.exclude(years__year__exact=datetime.datetime.now().year).order_by('stage__number', 'number'))
+    block = forms.ModelChoiceField(queryset=TeachingBlock.objects.exclude(years__year__exact=datetime.datetime.now().year).order_by('stage__number', 'code'))
     year = forms.IntegerField(initial=datetime.datetime.now().year, widget=bsforms.StaticControl())
     start = forms.DateField(widget=forms.TextInput(), help_text="The first day that students can assign themselves to activities in this block.", label="Start date")
     end = forms.DateField(widget=forms.TextInput(), help_text="The last day that students can assign themselves to activities in this block.", label="End date")
@@ -191,7 +278,7 @@ class NewTeachingBlockDetailsForm(bsforms.BootstrapHorizontalModelForm):
 class TeachingBlockValidationForm(bsforms.BootstrapHorizontalModelForm):
     class Meta:
         model = TeachingBlock
-        exclude = ('stage', 'number', 'start', 'end')
+        exclude = ('stage', 'code', 'start', 'end')
 
 
 class NewQuizSpecificationForm(bsforms.NewBootstrapModelForm):
@@ -218,3 +305,26 @@ class CommentForm(bsforms.NewBootstrapModelForm):
 
     class Meta:
         model = Comment
+
+
+# class ReasonForFlaggingForm(bsforms.NewBootstrapModelForm):
+#     body = forms.CharField(widget=forms.Textarea(), label="Reason")
+#     question = forms.ModelChoiceField(queryset=Question.objects.all(), widget=forms.HiddenInput())
+#     reason_type = forms.ChoiceField(choices=Reason.REASON_TYPES, widget=forms.HiddenInput())
+#     creator = forms.ModelChoiceField(queryset=Student.objects.all(), widget=forms.HiddenInput())
+
+#     class Meta:
+#         model = Reason
+
+class ReasonForFlaggingForm(bsforms.NewBootstrapForm):
+    reason = forms.CharField(widget=forms.Textarea())
+
+class QuestionAttributesForm(bsforms.NewBootstrapModelForm):
+    suitable_for_faculty = forms.BooleanField(widget=bsforms.CheckboxInput(), label="This question is suitable for submission to the Faculty for consideration as official exam material.", required=False)
+    suitable_for_quiz = forms.BooleanField(widget=bsforms.CheckboxInput(), label="This question should be included in the model quiz for this block.", required=False)
+    requires_special_formatting = forms.BooleanField(widget=bsforms.CheckboxInput(), label="This question requires special formatting.", required=False)
+
+    class Meta:
+        model = Question
+        exclude = ('body', 'options', 'answer', 'explanation', 'date_created', 'creator', 'approver', 'teaching_activity_year', 'status')
+
